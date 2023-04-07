4ff4b6f8b980ff5af74396d86edf23ad29b9df67
Swaraa1211 <saiswaroopasaranath1211@gmail.com>
2023-04-07 15:25:28 +0530

    Modified

--- CollegeSportsManagementSystem.cs
+++ CollegeSportsManagementSystem.cs
@@ -10,33 +10,85 @@ namespace Assessment3Apr7
 
         static void Main(string[] args)
         {
+            SqlConnection sqlConnection = new SqlConnection(connString);
+            sqlConnection.Open();
+
+            Console.WriteLine("Welcome!");
+
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("Your Choice: ");
+                Console.WriteLine("1. Add Tournament");
+                Console.WriteLine("2. Add Sports");
+                Console.WriteLine("3. Add Score");
+                Console.WriteLine("4. Edit Score");
+                Console.WriteLine("5. Remove Players");
+                Console.WriteLine("6. Remove Tournament");
+                Console.WriteLine("7. Remove Sports");
+                Console.WriteLine("8. Exit");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddTournament(sqlConnection);
+                        break;
+                    case 2:
+                        AddSports(sqlConnection);
+                        break;
+                    case 3:
+                        AddScore(sqlConnection);
+                        break;
+                    case 4:
+                        EditScore(sqlConnection);
+                        break;
+                    case 5:
+                        RemovePlayers(sqlConnection);
+                        break;
+                    case 6:
+                        RemoveTournament(sqlConnection);
+                        break;
+                    case 7:
+                        RemoveSports(sqlConnection);
+                        break;
+                    case 8:
+                        sqlConnection.Close();
+                        Console.WriteLine("Thank You !");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+            } while (choice != 8);
             //AddTournament();
             //AddSports();
             //AddScore();
             //EditScore();
-            RemovePlayers();
+            //RemovePlayers();
             //RemoveTournament();
             //RemoveSports();
             
-            
         }
 
-        public static void AddTournament()
+        public static void AddTournament(SqlConnection sqlConnection)
         {
-            SqlConnection connection = new SqlConnection(connString);
-            connection.Open();
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM SportsTournament";
+            string query = "SELECT * FROM SportsTournament";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
 
             //Inserting
             try
             {
+                Console.Write("Tournament Number: ");
                 int tn = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nTournament Name: ");
                 string Tname = Console.ReadLine();
+                Console.Write("\nSport1: ");
                 string Sport1 = Console.ReadLine();
+                Console.Write("\nSport2: ");
                 string Sport2 = Console.ReadLine();
 
-                Console.WriteLine("TournamentNumber\t\tTournamentName\t\tSport1\t\tSport2");
                 int count = (int)cmd.ExecuteScalar();
                 if (count > 0)
                 {
@@ -44,10 +96,6 @@ namespace Assessment3Apr7
                                   $"select * from SportsTournament";
                     cmd.ExecuteReader().Close();
                     SqlDataReader rd = cmd.ExecuteReader();
-                    while (rd.Read())
-                    {
-                        Console.WriteLine($"{rd.GetInt32(0)}\t\t{rd.GetString(1)}\t\t{rd.GetString(2)}\t\t{rd.GetString(3)}");
-                    }
                 }
                 else
                 {
@@ -59,23 +107,25 @@ namespace Assessment3Apr7
                 Console.WriteLine($"Error: {e.Message}");
             }
 
-            connection.Close();
+            Console.WriteLine("Tournament Added Successfully!");
 
         }
 
-        public static void AddSports()
+        public static void AddSports(SqlConnection sqlConnection)
         {
-            SqlConnection connection = new SqlConnection(connString);
-            connection.Open();
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM Sports";
+            string query = "SELECT * FROM Sports";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
 
             //Inserting
             try
             {
+                Console.Write("Tournament Number: ");
                 int tn = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nSport Id: ");
                 int sportId = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nSport Name: ");
                 string SportName = Console.ReadLine();
+                Console.Write("\nCategory: ");
                 string Category = Console.ReadLine();
 
                 Console.WriteLine("TournamentNumber\t\tSportID\t\tSportName\t\tCategory");
@@ -101,24 +151,27 @@ namespace Assessment3Apr7
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
-            connection.Close();
+            Console.WriteLine("Successfully Added!");
         }
 
-       public static void AddScore()
+       public static void AddScore(SqlConnection sqlConnection)
         {
-            SqlConnection connection = new SqlConnection(connString);
-            connection.Open();
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM ScoreCard";
+            string query = "SELECT * FROM ScoreCard";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
 
 
             //Removing
             try
             {
+                Console.Write("Athlete Id: ");
                 int Id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nAthlete Name: ");
                 string Name = Console.ReadLine();
+                Console.Write("\nSport Id: ");
                 int sportId = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nAge: ");
                 int age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nScore: ");
                 int score = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("AtheteId\t\tAthleteName\t\tSportId\t\tAge\t\tScore");
@@ -144,22 +197,20 @@ namespace Assessment3Apr7
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
-
-            connection.Close();
+            Console.WriteLine("Score Added!");
         }
 
-        public static void EditScore()
+        public static void EditScore(SqlConnection sqlConnection)
         {
-            SqlConnection connection = new SqlConnection(connString);
-            connection.Open();
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM ScoreCard";
-
+            string query = "SELECT * FROM ScoreCard";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
 
             //Removing
             try
             {
+                Console.Write("Athlete Id: ");
                 int Id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nScore: ");
                 int score = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("AtheteId\t\tAthleteName\t\tSportId\t\tAge\t\tScore");
@@ -185,17 +236,14 @@ namespace Assessment3Apr7
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
-
-            connection.Close();
+            Console.WriteLine("Score Updated!");
         }
 
-        public static void RemovePlayers()
+        public static void RemovePlayers(SqlConnection sqlConnection)
         {
-            SqlConnection connection = new SqlConnection(connString);
-            connection.Open();
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM ScoreCard";
-
+            string query = "SELECT * FROM ScoreCard";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            Console.Write("Player Name: ");
             string playerName = Console.ReadLine();
 
             //Removing
@@ -224,16 +272,14 @@ namespace Assessment3Apr7
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
-
-            connection.Close();
+            Console.WriteLine("Removed!!");
         }
-        public static void RemoveTournament()
+        public static void RemoveTournament(SqlConnection sqlConnection)
         {
-            SqlConnection connection = new SqlConnection(connString);
-            connection.Open();
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM SportsTournament";
+            string query = "SELECT * FROM SportsTournament";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
 
+            Console.Write("Tournament Number: ");
             int tn = Convert.ToInt32(Console.ReadLine());
 
             //Removing
@@ -262,18 +308,15 @@ namespace Assessment3Apr7
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
-
-            connection.Close();
-
+            Console.WriteLine("Tournament Removed!");
         }
 
-        public static void RemoveSports()
+        public static void RemoveSports(SqlConnection sqlConnection)
         {
-            SqlConnection connection = new SqlConnection(connString);
-            connection.Open();
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM Sports";
+            string query = "SELECT * FROM Sports";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
 
+            Console.Write("SportId: ");
             int sportId = Convert.ToInt32(Console.ReadLine());
 
             //Removing
@@ -302,60 +345,8 @@ namespace Assessment3Apr7
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
-
-            connection.Close();
+            Console.WriteLine("Sport Removed!");
         }
     }
 }
 
-
-//SqlConnection connection = new SqlConnection(connString);
-//connection.Open();
-
-//SqlCommand command = connection.CreateCommand();
-//command.CommandText = "SELECT * FROM SportsTournament";
-//SqlDataReader reader = command.ExecuteReader();
-
-///*Console.WriteLine("TounamentNum         Name");
-//while (reader.Read())
-//{
-//    Console.WriteLine($"{reader.GetInt32(0)}      {reader.GetString(1)}");
-//}*/
-
-////SqlDataReader reader = cmd.ExecuteReader();
-////cmd.CommandText = "SELECT * FROM orderDetails";
-//Console.WriteLine("Number       Name     Sport1  Sport2");
-//while (reader.Read())
-//{
-//    Console.WriteLine($"{reader.GetInt32(0)}      {reader.GetString(1)}          {reader.GetString(2)}      {reader.GetString(3)}");
-//}
-
-//reader.Close();
-
-//connection.Close();
-
-//UpdationinAddTournament
-//try
-//{
-//    int tn1 = Convert.ToInt32(Console.ReadLine());
-//    cmd.CommandText = $"select count(*) from SportsTournament where TournamentNumber={tn1}";
-//    int count = (int)cmd.ExecuteScalar();
-//    if (count > 0)
-//    {
-//        cmd.CommandText = $"UPDATE SportsTournament set TournamentName = 'VCETIntraCollege' where TournamentNumber = {tn1}" +
-//            $"Select * from SportsTournament where TournamentNumber = {tn1}";
-//        cmd.ExecuteReader().Close();
-//        SqlDataReader rd = cmd.ExecuteReader();
-//        while (rd.Read())
-//        {
-//            Console.WriteLine($"{rd.GetInt32(0)}\t\t{rd.GetString(1)}\t\t{rd.GetString(2)}\t\t{rd.GetString(3)}");
-//        }
-//    }
-//    else
-//    {
-//        Console.WriteLine("the number doesnot exist");
-//    }
-//}
-//catch (SqlException e)
-//{
-//    Console.WriteLine($"Error: {e.Message}");
